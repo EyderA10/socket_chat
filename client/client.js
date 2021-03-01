e2ccb02d8a3d8da2140a01b29006ed08bfe2d849
EyderA10 <eyderalejandro81@gmail.com>
2021-03-01 09:54:34 -0400

    corrijiendo errores

--- client/client.js
+++ client/client.js
@@ -1,61 +1,65 @@
-const socket = io.connect("https://n0wld.sse.codesandbox.io/", {
-  forceNew: true
+const socket = io.connect(`http://localhost:4000/`, {
+  'forceNew': true
 });
 
-socket.on("messages", (data) => {
+socket.on('messages', (data) => {
   addMessages(data);
 });
 
 const addMessages = (data) => {
-  const divMessages = document.getElementById("messages");
+  const divMessages = document.getElementById('messages');
 
   const messages = data
     .map(
       ({ description, nickname }) =>
         `
-      <div class="block-m">
-          <p id="nick" class="nickname"><strong>${nickname}:</strong> <span class="description">${description}</span></p>
+      <div class='block-m'>
+          <p id='nick' class='nickname'><strong>${nickname} </strong> <span class='description'>${description}</span></p>
       </div>
     `
     )
-    .join(" ");
+    .join(' ');
 
-  divMessages.style.width = "70%";
-  divMessages.style.border = "1px solid black";
-  divMessages.style.marginBottom = "10px";
-  divMessages.style.padding = "5px";
+  divMessages.style.width = '40%';
+  divMessages.style.border = '1px solid black';
+  divMessages.style.marginBottom = '10px';
+  divMessages.style.padding = '5px';
   divMessages.innerHTML = messages;
   divMessages.scrollTop = divMessages.scrollTopHeight;
 };
 
 const handleInput = () => {
-  const description = document.getElementById("message").value;
+  const description = document.getElementById('message').value;
 
-  const nickname = document.getElementById("nickname").value;
+  const nickname = document.getElementById('nickname').value;
 
-  const send = document.getElementById("send");
-
-  if (description.trim().length === 0 || nickname.trim().length === 0) {
-    send.setAttribute("disabled", "true");
+  const send = document.getElementById('send');
+  
+  if (nickname.trim().length === 0) {
+    send.setAttribute('disabled', 'true');
   } else {
-    send.removeAttribute("disabled");
+    send.removeAttribute('disabled');
   }
+
+  if (description.trim().length === 0) {
+    send.setAttribute('disabled', 'true');
+  } else {
+    send.removeAttribute('disabled');
+  }
+
 };
 
-const form = document.getElementById("chatSubmit");
-
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  const description = document.getElementById("message").value;
-
-  const nickname = document.getElementById("nickname").value;
-
+document.getElementById('chatSubmit').addEventListener('submit', function (e) {
+  e.preventDefault()
   const message = {
-    description,
-    nickname
+    description: document.getElementById('message').value,
+    nickname: document.getElementById('nickname').value
   };
 
-  nickname.style.display = "none";
-  socket.emit("chatM", message);
-  form.reset();
+  document.getElementById('nickname').style.display = 'none';
+  socket.emit('chatM', message);
+  this.reset();
+  return false;
 });
+
+
--- server/server.js
+++ server/server.js
@@ -1,47 +1,50 @@
 //lib
-const express = require("express");
-const { createServer } = require("http");
+const express = require('express');
+const { createServer } = require('http');
 
 //initializase
 const app = express();
+const port = process.env.PORT || 4000;
 const server = createServer(app);
 
 //middleware
-app.use(express.static("client"));
+app.use(express.static('client'));
 
 //socket
-const io = require("socket.io")(server);
+const io = require('socket.io')(server);
 
 const info = [
   {
     id: 1,
-    description: "Welcome to my chat",
-    nickname: "EyderDev"
+    description: 'Welcome to my chat',
+    nickname: 'EyderDev'
   }
 ];
 
-io.on("connection", (socket) => {
+io.on('connection', (socket) => {
   console.log(`El usuario con ip:${socket.handshake.address} se ha conectado!`);
-  socket.emit("messages", info);
+  socket.emit('messages', info);
 
-  io.on("disconnect", () => {
+  io.on('disconnect', () => {
     console.log(
       `El usuario con ip:${socket.handshake.address} se ha desconectado!`
     );
   });
 
-  socket.on("chatM", (message) => {
+  socket.on('chatM', (message) => {
     info.push(message);
 
-    io.sockets.emit("messages", info);
+    io.sockets.emit('messages', info);
   });
 });
 
 //server
-app.get("/", (req, res) => {
-  res.status(200).send("<h1>Working</h1>");
+app.get('/', (req, res) => {
+  res.status(200).send('<h1>Working</h1>');
 });
 
-server.listen({ port: null }, () => {
-  console.log(`Server runnig on https://n0wld.sse.codesandbox.io/`);
+server.listen(port, () => {
+  console.log(`Server runnig on http://localhost:${port}/`);
 });
+
+module.exports = port;
